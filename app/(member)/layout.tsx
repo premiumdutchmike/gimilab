910e637184c8b6cd4460f4090116bb8a7996e425
dutchmike-4901 <dutchmike@remodelingconcepts.net>
2026-03-20 08:16:47 -0400

    feat: add MemberNav with credits to member layout

--- app/(member)/layout.tsx
+++ app/(member)/layout.tsx
@@ -1,5 +1,7 @@
 import { redirect } from 'next/navigation'
 import { createClient } from '@/lib/supabase/server'
+import { getCreditBalance } from '@/lib/credits/ledger'
+import MemberNav from '@/components/member-nav'
 
 export default async function MemberLayout({
   children,
@@ -13,5 +15,22 @@ export default async function MemberLayout({
     redirect('/login')
   }
 
-  return <>{children}</>
+  let balance = 0
+  try {
+    balance = await getCreditBalance(user.id)
+  } catch (err) {
+    console.error('[member-layout] getCreditBalance failed', err)
+  }
+
+  const firstName =
+    user.user_metadata?.full_name?.split(' ')[0] ??
+    user.email?.split('@')[0] ??
+    'You'
+
+  return (
+    <div className="min-h-screen bg-[#090f1a] flex flex-col">
+      <MemberNav credits={balance} firstName={firstName} />
+      <main className="flex-1">{children}</main>
+    </div>
+  )
 }
